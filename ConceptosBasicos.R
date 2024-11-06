dab8c5e33a454be163e2500ea27750a1aa3446ed
Angelmbx <mirandabl.angel@gmail.com>
2024-11-06 20:41:15 +0000

    Commit inicial

---
+++ ConceptosBasicos.R
@@ -0,0 +1,12 @@
+a = 1+1
+b = 3*a
+c = b/a
+d = sqrt(b) # sqrt = raíz cuadrada
+?sqrt # ? proporciona documentación sobre el elemento que venga después - en este caso sqrt -
+
+# para conocer el tipo de dato
+class(d) # [1] "numeric"
+
+# convertir a tipo character
+b = as.character(5)
+class(b) # [1] "character"
