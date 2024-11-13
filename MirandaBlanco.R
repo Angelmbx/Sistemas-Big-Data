60497e845596144c8e6d3dc76ff751706e90cfe9
Angelmbx <miranda.bl@gmail.com>
2024-11-13 12:35:04 +0100

    Ejercicio 2 finalizado

--- MirandaBlanco.R
+++ MirandaBlanco.R
@@ -1,7 +1,7 @@
-# Tarea 1 - Carga de datps desde fichero XLSX
+## Tarea 1 - Carga de datps desde fichero XLSX
 
 # install.packages("readxl")
-# library(readxl)
+library(readxl)
 # Para ejecutar script -> R en terminal , despues source('nombredearchivo')
 
 
@@ -12,9 +12,27 @@ hojas_disponibles <- excel_sheets("EstadísticasNetflix.xlsx")
 
 if (nombre_hoja %in% hojas_disponibles) {
   datos_excel <- read_excel(ruta_archivo, sheet = nombre_hoja)
+  df = data.frame(datos_excel)
+  print(df)
   
-  print(datos_excel)
 } else {
   # Imprime un mensaje si la hoja no está presente
   cat("La hoja", nombre_hoja, "no está presente en el archivo Excel.\n")
 }
+
+## Tarea 2. Extracción de subconjunto
+#a) Filas de los años comprendidos entre 2015 y 2016 inclusive
+
+years_a <- df[(5:6),]
+View(years_a)
+
+#b) Valores de los años 2018 y 2020
+
+years_b <- df[c(8,10),]
+View(years_b)
+
+#c) Los tres primeros registros
+
+years_c <- df[1:3,]
+View(years_c)
+
